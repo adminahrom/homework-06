47c3c40e484b8772e253e87943fc7f55681e1c25
adminahrom <adminahrom@mail.ru>
2021-11-09 12:33:32 +0300

    Initial commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import NavBar from "./components/navBar";
+import { Route, Switch } from "react-router-dom";
 import Users from "./components/users";
+import Login from "./components/login";
+import Main from "./components/main";
+import User from "./components/user";
 function App() {
-    return <Users />;
+    return (
+        <div>
+            <NavBar />
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/login" component={Login} />
+                <Route path="/users/:userId" component={User} />
+                <Route path="/users" component={Users} />
+            </Switch>
+            <Users />
+        </div>
+    );
 }
 
 export default App;
